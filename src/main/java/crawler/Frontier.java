1f3b610a8dad565398c53387be69dcf8ee6df28a
DESKTOP-NEPNL3H\Samsung <liguqu168@gmail.com>
2018-10-21 12:42:54 +0700

    Add function check over depth trap

--- src/main/java/crawler/Frontier.java
+++ src/main/java/crawler/Frontier.java
@@ -6,6 +6,7 @@
 package crawler;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.PriorityQueue;
 
@@ -18,7 +19,7 @@ public class Frontier {
     public static PriorityQueue urlQueue = new PriorityQueue();
     public static HashSet<String> crawledUrl = new HashSet<String>();
     public static ArrayList<String> fileTypes =  new ArrayList<String>();
-
+    public static int MAX_URL_DEPTH = 16;
     public static boolean shouldVisit(String url) {
 
         if (checkCrawledUrl(url)) {
@@ -33,6 +34,14 @@ public class Frontier {
     }
     
     private static boolean checkTraps(String url){
+        return isURLOverDepth(url);
+    }
+    
+        public static boolean isURLOverDepth(String url) {
+        String[] temp = url.split("/");
+        if (temp.length > MAX_URL_DEPTH) {
+            return true;
+        }
         return false;
     }
     
